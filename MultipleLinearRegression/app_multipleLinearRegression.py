e604c7612c30d3db6b5e90b58d433e788fb61fed
AmulyaGaje <amulyagaje5@gmail.com>
2025-12-18 14:19:39 +0530

    Add files via upload

---
+++ MultipleLinearRegression/app_multipleLinearRegression.py
@@ -0,0 +1,104 @@
+import streamlit as st
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error,mean_squared_error,r2_score,root_mean_squared_error
+
+st.set_page_config("Multiple Linear Regression",layout="centered")
+
+#Load CSS
+def load_css(file):
+    with open(file) as f:
+        st.markdown(f"<style>{f.read()}</style>",unsafe_allow_html=True)
+load_css("style.css")
+
+#Title
+st.markdown("""
+<div class="card">
+            <h1>MULTIPLE LINEAR REGRESSION</h1>
+            <p>Pedict <b>Tip Amount</b> from the <b>Total Bill </b>using Multiple Linear Regression..</p>
+            </div>
+
+""",unsafe_allow_html=True)
+
+@st.cache_data
+def load_data():
+    return sns.load_dataset("tips")
+df=load_data()
+#Dataset preview
+st.markdown('<div class="card">',unsafe_allow_html=True)
+st.subheader("DataSet Preview")
+st.dataframe(df[["total_bill","size","tip"]].head())
+st.markdown('</div>',unsafe_allow_html=True)
+#Prepare Dataset
+x,y=df[["total_bill","size"]],df["tip"]
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
+scaler=StandardScaler()
+x_train_scaled=scaler.fit_transform(x_train)
+x_test_scaled=scaler.transform(x_test)
+#Train the model
+model=LinearRegression()
+model.fit(x_train,y_train)
+y_pred=model.predict(x_test)
+#Metrics
+mae=mean_absolute_error(y_test,y_pred)
+mse=mean_squared_error(y_test,y_pred)
+rmse=root_mean_squared_error(y_test,y_pred)
+r2_score=r2_score(y_test,y_pred)
+adj_r2=1-(1-r2_score)*(len(y_test)-1)/(len(y_test)-2)
+#visualization
+st.markdown('<div class="card">',unsafe_allow_html=True)
+st.subheader("Total Bills vs Tip")
+fig,ax=plt.subplots()
+ax.scatter(df['total_bill'],df["tip"],alpha=0.6)
+ax.plot(df["total_bill"],model.predict(scaler.transform(x)),color="red")
+ax.set_xlabel("Total Bill ")
+ax.set_ylabel("Tip ")
+st.pyplot(fig)
+st.markdown('</div',unsafe_allow_html=True)
+#perfomance metrics
+st.markdown('<div class="card">',unsafe_allow_html=True)
+st.header("Model Perfomance")
+c1,c2,c3=st.columns(3)
+c1.metric("MAE",f"{mae:.2f}")
+c2.metric("RMSE",f"{rmse:.2f}")
+c3.metric("MSE",f"{mse:.2f}")
+c4,c5,c6=st.columns(3)
+c4.metric("R2",f"{r2_score:.3f}")
+c5.metric("Adjusted R2",f"{adj_r2:.3f}")
+st.markdown('</div>',unsafe_allow_html=True)
+#m&c
+st.markdown(f"""
+        <div class="card">
+        <h3>Model Interception</h3>
+        <p><b>Co-efficent:(total bill)</b>{model.coef_[0]:.3f}<br>
+        <p><b>Co-efficient(Group size)</b>{model.coef_[1]:.3f}<br>
+        <p><b>Intercept:</b>{model.intercept_:.3f}</p>
+        <p>Tip depends upon the<b>Bill Amount</b> and <b>number of people</b>
+        </p>
+
+</div>
+""",unsafe_allow_html=True)
+# Prediction
+st.markdown('<div class="card">', unsafe_allow_html=True)
+st.subheader("Predict Tip Amount")
+
+bill = st.slider(
+    "Total Bill",
+    float(df["total_bill"].min()),
+    float(df["total_bill"].max()),
+    30.0
+)
+size=st.slider("Group size",int(df["size"].min()),int(df["size"].max()),2)
+input_scaled=scaler.transform([[bill,size]])
+tip = model.predict(input_scaled)[0]
+
+st.markdown(
+    f'<div class="prediction-box">Predicted Tip: {tip:.2f}</div>',
+    unsafe_allow_html=True
+)
+
+st.markdown('</div>', unsafe_allow_html=True)
